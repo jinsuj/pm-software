16be9982e65ab4a098011959bcdb907a46e5312a
Jovanny Frias <Jovanny.Frias@fdmgroup.com>
2020-11-02 14:52:21 -0600

    Initial project

---
+++ Stratagem/src/test/java/com/fdmgroup/Stratagem/StratagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.Stratagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StratagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
