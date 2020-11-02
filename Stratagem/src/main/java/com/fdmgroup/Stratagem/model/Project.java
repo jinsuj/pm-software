f59d968b653c810a250ede1a68373cb9ea033773
austin.harris <austin.harris@fdmgroup.com>
2020-11-02 22:24:33 +0000

    Initial Commit

---
+++ Stratagem/src/main/java/com/fdmgroup/Stratagem/model/Project.java
@@ -0,0 +1,77 @@
+package com.fdmgroup.Stratagem.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+@Entity
+@Table(name="PROJECTS")
+public class Project implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="PROJECT_ID")
+	private int id;
+	
+	@Column(name="PROJECT_NAME")
+	private String name;
+	
+	@Column(name="PROJECT_DESCRIPTION")
+	private String description;
+	
+	public Project() {
+		super();
+	}
+
+	public Project(int id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Project [id=" + id + ", name=" + name + ", description=" + description + "]";
+	}
+
+}
---
+++ Stratagem/src/main/java/com/fdmgroup/Stratagem/repository/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.fdmgroup.Stratagem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fdmgroup.Stratagem.model.Project;
+
+public interface ProjectRepository extends JpaRepository<Project,Integer>{
+	
+
+}
