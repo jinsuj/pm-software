2ef445ce4e8789b397a24cf92493151c667f6141
Ibrahim Jabr <ibrahim.aljabr93@gmail.com>
2020-11-02 16:12:53 -0600

    creating admin controler and create/delete project

---
+++ Stratagem/src/main/java/com/fdmgroup/controllers/AdminController.java
@@ -0,0 +1,62 @@
+package com.fdmgroup.controllers;
+
+import java.util.Optional;
+
+import javax.persistence.TransactionRequiredException;
+
+import org.hibernate.id.IdentifierGenerationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.jpa.JpaSystemException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fdmgroup.IssueTracker.repositories.CustomerRepo;
+
+
+
+@RestController
+@CrossOrigin("*")
+public class AdminController {
+
+	@Autowired
+	private ProjectRepo projectRepo;
+	
+	@PostMapping("/createProject")
+	public boolean createProject(@RequestBody Project project) {
+		System.out.println(project.toString());
+		try {
+			projectRepo.save(project);
+			return true;
+			}
+		catch (TransactionRequiredException e) {
+			e.printStackTrace();
+		} catch (IdentifierGenerationException e) {
+			e.printStackTrace();
+		} catch (JpaSystemException e) {
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+	
+	@PostMapping("/deleteProject")
+	public boolean deleteProject(@RequestBody Project project) {
+		System.out.println(project.toString());
+		try {
+			projectRepo.delete(project);
+			return true;
+			}
+		catch (TransactionRequiredException e) {
+			e.printStackTrace();
+		} catch (IdentifierGenerationException e) {
+			e.printStackTrace();
+		} catch (JpaSystemException e) {
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+	
+}
