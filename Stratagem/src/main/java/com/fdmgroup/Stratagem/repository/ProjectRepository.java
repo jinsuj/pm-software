99e38bcad7cbdba834ac3b960c8498a76941838e
austin.harris <austin.harris@fdmgroup.com>
2020-11-03 18:00:06 +0000

    Updated ProjectRepository

--- Stratagem/src/main/java/com/fdmgroup/Stratagem/repository/ProjectRepository.java
+++ Stratagem/src/main/java/com/fdmgroup/Stratagem/repository/ProjectRepository.java
@@ -1,10 +1,16 @@
 package com.fdmgroup.Stratagem.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.fdmgroup.Stratagem.model.Project;
 
 public interface ProjectRepository extends JpaRepository<Project,Integer>{
 	
+	@Query("select p from Project p where upper(p.name) like concat('%', upper(:name), '%')")
+	public List<Project> findByName(@Param("name") String name);
 
 }
