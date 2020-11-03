80dd6334eaad7194b9f3043ed9f76ff52d9bd0dc
mgla229 <michaellawson229@gmail.com>
2020-11-03 14:37:41 -0500

    Added missing import

--- Stratagem/src/main/java/com/fdmgroup/Stratagem/controllers/AdminController.java
+++ Stratagem/src/main/java/com/fdmgroup/Stratagem/controllers/AdminController.java
@@ -7,6 +7,7 @@ import org.hibernate.id.IdentifierGenerationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.jpa.JpaSystemException;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
